43848f22e1badb4b9a2d31a6bd731625d9b0c5bd
beanzhou <zhoubinbeihang@163.com>
2018-04-26 16:19:06 +0800

    wx login init

--- Android/app/src/main/java/com/beanzhou/login/login/MainActivity.java
+++ Android/app/src/main/java/com/beanzhou/login/login/MainActivity.java
@@ -8,22 +8,40 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.tencent.mm.sdk.openapi.BaseReq;
+import com.tencent.mm.sdk.openapi.BaseResp;
+import com.tencent.mm.sdk.openapi.IWXAPI;
+import com.tencent.mm.sdk.openapi.IWXAPIEventHandler;
+import com.tencent.mm.sdk.openapi.SendAuth;
+import com.tencent.mm.sdk.openapi.WXAPIFactory;
 import com.tencent.tauth.IUiListener;
 import com.tencent.tauth.Tencent;
 import com.tencent.tauth.UiError;
 
-import org.json.JSONObject;
+/**
+ *  微信：
+ *  AppID: wx38684064dbf9ddca
+ *  AppSecret: 77f4c7ee709f2199ab27e176aa1726bf
+ *
+ *  QQ:
+ *  APP ID: 1106786249APP
+ *  KEY: JZWlfoQaWI1DtVQn
+ */
+public class MainActivity extends AppCompatActivity implements IWXAPIEventHandler{
 
-public class MainActivity extends AppCompatActivity {
 
+    final static String WX_APP_ID = "wx38684064dbf9ddca";
+    final static String WX_APP_SECRET = "77f4c7ee709f2199ab27e176aa1726bf";
 
-    final static String WXAPP_SECRET = "77f4c7ee709f2199ab27e176aa1726bf";
+    final static String QQ_APP_ID = "1106786249";
+    final static String QQ_APP_SECRET = "JZWlfoQaWI1DtVQn";
 
-    private Button button;
-    private TextView text1, text2;
-    private Tencent mTencent;
+    private Button      buttonQQLogin, buttonWXLogin;
+    private TextView    text1, text2;
 
-    private IUiListener Listener = new IUiListener() {
+    // QQ
+    private Tencent     mTencent;
+    private IUiListener QQLoginListener = new IUiListener() {
         @Override
         public void onComplete(Object response) {
             String ss = response.toString();
@@ -42,6 +60,25 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
+    public void qqLogin() {
+        mTencent.login(this, "all", QQLoginListener);
+    }
+
+
+    // wx
+    private IWXAPI mWxApi;
+
+    private void regToWx() {
+        mWxApi = WXAPIFactory.createWXAPI(this, MainActivity.WX_APP_ID, true);
+        mWxApi.registerApp(MainActivity.WX_APP_ID);
+    }
+
+    public void wxLogin() {
+        final SendAuth.Req req = new SendAuth.Req();
+        req.scope = "snsapi_userinfo";
+        req.state = "wx_state_login";
+        mWxApi.sendReq(req);
+    }
 
 
     @Override
@@ -50,16 +87,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTencent = Tencent.createInstance("1106786249", this.getApplicationContext());
+        mTencent = Tencent.createInstance(MainActivity.QQ_APP_ID, this.getApplicationContext());
 
         text1 = (TextView) findViewById(R.id.textView2);
         text2 = (TextView) findViewById(R.id.textView4);
 
-        button = (Button)findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-
-
-
+        buttonQQLogin = (Button)findViewById(R.id.button_qq_login);
+        buttonQQLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 MainActivity.this.qqLogin();
@@ -69,14 +103,31 @@ public class MainActivity extends AppCompatActivity {
                 text1.setText(chars, 0, chars.length);
             }
         });
+
+        regToWx();
+        buttonWXLogin = (Button) findViewById(R.id.button_wx_login);
+        buttonWXLogin.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                MainActivity.this.wxLogin();
+            }
+        });
+
     }
 
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        Tencent.onActivityResultData(requestCode, resultCode, data,Listener);
+        Tencent.onActivityResultData(requestCode, resultCode, data, QQLoginListener);
     }
 
-    public void qqLogin() {
-        mTencent.login(this, "all", Listener);
+    @Override
+    public void onReq(BaseReq baseReq) {
+
+    }
+
+    @Override
+    public void onResp(BaseResp baseResp) {
+
     }
 }
 
